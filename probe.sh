de427e3f82326107f21c14390a8ece02fa1abf0e
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-11-20 12:20:33 +0800

    add

--- probe.sh
+++ probe.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/env sh
 HERE=`cd $(dirname $0); pwd`
-ESXI_VMS=$(vim-cmd vmsvc/getallvms | sed '1d;s/ .*$//')
+LOGFILE=/tmp/esxi.log
+HBIP=192.168.0.10
 
-echo $HERE
-echo $ESXI_VMS
+function list_vms() {
+  vim-cmd vmsvc/getallvms | sed '1d;s/ .*$//'
+}
+
+function check_vm_state() {
+  vim-cmd vmsvc/power.getstate $1 | grep 'Powered on'
+}
+
+function poweroff_vm() {
+  echo "vim-cmd vmsvc/power.off $1" >> $LOGFILE
+}
+
+echo "$(date) start" >> $LOGFILE
+# do power off
+for vmid in $(list_vms); do
+  power_state=$(check_vm_state $vmid)
+  if [ -n "$power_state" ]; then
+    poweroff_vm $vmid
+    sleep 1
+  fi
+done
+
+echo "$(date) finish" >> $LOGFILE
