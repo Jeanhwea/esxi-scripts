01a58f60e4a489c1a59096b4fc5599adb9136ffd
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-11-20 13:06:43 +0800

    add

--- probe.sh
+++ probe.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 HERE=`cd $(dirname $0); pwd`
 HBIP=192.168.0.10
-
-FILETAG=$(date +'%Y%m%d.%H%M%S')
+FILETAG=$(date +'%Y%m%d')
 LOGFILE="/tmp/esxi.log.$FILETAG"
 
 function log_prefix() {
@@ -22,14 +21,25 @@ function poweroff_vm() {
   echo "$(log_prefix) vim-cmd vmsvc/power.off $1" >> $LOGFILE
 }
 
-echo "$(log_prefix) start" >> $LOGFILE
-# do power off
-for vmid in $(list_vms); do
-  power_state=$(check_vm_state $vmid)
-  if [ -n "$power_state" ]; then
-    poweroff_vm $vmid
-    sleep 1
-  fi
-done
+function test_heartbeats() {
+  ping -c 1 $HBIP > /dev/null
+  retval=$?
+  echo $retval
+}
 
+
+echo "$(log_prefix) start" >> $LOGFILE
+if [ $(test_heartbeats) -eq 0 ]; then
+  echo "$(log_prefix) $HBIP is alive"
+else
+  echo "$(log_prefix) $HBIP is dead"
+  # do power off
+  for vmid in $(list_vms); do
+    power_state=$(check_vm_state $vmid)
+    if [ -n "$power_state" ]; then
+      poweroff_vm $vmid
+      sleep 1
+    fi
+  done
+fi
 echo "$(log_prefix) finish" >> $LOGFILE
